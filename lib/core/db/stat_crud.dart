161f91fc2bb635fc45a9f818a002ec0a78b38769
sewaustav <sewabobrov04@gmail.com>
2025-10-04 22:01:44 +0200

    refactor stat methods

--- lib/core/db/stat_crud.dart
+++ lib/core/db/stat_crud.dart
@@ -1,90 +1,73 @@
-import 'package:dot_music/core/config.dart';
-import 'package:dot_music/core/db/db.dart';
-import 'package:dot_music/core/db/db_helper.dart';
+// stat_repository.dart
 import 'package:sqflite/sqflite.dart';
 
-class StatCrud {
-  Future<Database> get _db async => await DatabaseHelper().db;
-  final DbHelper _dbHelper = DbHelper();
-  final ss = StatService();
+class StatRepository {
+  final Database db;
 
-  // Increase count
-  Future<void> increaseCount(int trackId) async {
-    final db = await _db;
-    int playbackCount = await ss.getPlaybackCount(trackId);
-    await db.rawUpdate(
-      """
-        UPDATE tracks
-        SET playback_count = ?
-        WHERE id = ?
-      """,
-      [playbackCount+1, trackId]
-    );
-    await increaseCountMonth(playbackCount+1);
-  }
+  StatRepository(this.db);
 
-  Future<void> increaseCountMonth(int trackId) async {
-    final db = await _db;
-    DateTime now = DateTime.now();
-    if (await ss.isTrackExist(trackId, now.month, now.year)) {
-      await db.rawUpdate(
-        '''
-          UPDATE listening_stat
-          SET playback_count = playback_count + 1
-          WHERE track_id = ? AND month = ? AND year = ?
-        ''',
-        [trackId, now.month, now.year]
-      );
-    } else {
-      await _createMonthlyRecord(trackId, now.month, now.year);
-    }
-    
-  }
-
-  Future<void> _createMonthlyRecord(int trackId, int month, int year) async {
-    final db = await _db;
-    await db.rawInsert(
-      '''
-        INSERT INTO listening_stat 
-        (track_id, month, year, playback_count)
-        VALUES (?, ?, ?, 1)
-      ''',
-      [trackId, month, year]
-    );
-  }
-
-  
-}
-
-
-class StatService {
-
-  Future<Database> get _db async => await DatabaseHelper().db;
 
   Future<int> getPlaybackCount(int trackId) async {
-    final db = await _db;
-
-    final count = await db.rawQuery(
-      """
-        SELECT playback_count FROM tracks WHERE id = ?
-      """,
-      [trackId]
+    final result = await db.rawQuery(
+      'SELECT playback_count FROM tracks WHERE id = ?',
+      [trackId],
     );
-
-    return count.first['playback_count'] as int;
-    
+    return result.first['playback_count'] as int;
   }
 
-  Future<bool> isTrackExist(int trackId, int month, int year) async {
-    final db = await _db;
-    final _responce = await db.rawQuery(
-      '''SELECT 1 FROM listening_stat WHERE track_id = ? AND month = ? AND year = ?''',
-      [trackId, month, year]
+  Future<void> updateTrackPlaybackCount(int trackId, int newCount) async {
+    await db.rawUpdate(
+      'UPDATE tracks SET playback_count = ? WHERE id = ?',
+      [newCount, trackId],
     );
-    return _responce.isNotEmpty;
+  }
+
+  Future<bool> monthlyStatExists(int trackId, int month, int year) async {
+    final res = await db.rawQuery(
+      'SELECT 1 FROM listening_stat WHERE track_id = ? AND month = ? AND year = ?',
+      [trackId, month, year],
+    );
+    return res.isNotEmpty;
+  }
+
+  Future<void> incrementMonthlyStat(int trackId, int month, int year) async {
+    await db.rawUpdate(
+      '''
+      UPDATE listening_stat
+      SET playback_count = playback_count + 1
+      WHERE track_id = ? AND month = ? AND year = ?
+      ''',
+      [trackId, month, year],
+    );
+  }
+
+  Future<void> createMonthlyStat(int trackId, int month, int year) async {
+    await db.rawInsert(
+      '''
+      INSERT INTO listening_stat (track_id, month, year, playback_count)
+      VALUES (?, ?, ?, 1)
+      ''',
+      [trackId, month, year],
+    );
   }
 }
 
-class CalcStat extends StatCrud {
-  
+class StatService {
+  final StatRepository repository;
+
+  StatService(this.repository);
+
+  Future<void> registerPlayback(int trackId) async {
+    final currentCount = await repository.getPlaybackCount(trackId);
+    await repository.updateTrackPlaybackCount(trackId, currentCount + 1);
+
+    final now = DateTime.now();
+    final exists = await repository.monthlyStatExists(trackId, now.month, now.year);
+    if (exists) {
+      await repository.incrementMonthlyStat(trackId, now.month, now.year);
+    } else {
+      await repository.createMonthlyStat(trackId, now.month, now.year);
+    }
+  }
 }
+
